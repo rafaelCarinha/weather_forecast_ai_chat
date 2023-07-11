2192fff60a6312f60520fe164b1c97cee509fc8e
Rafael Marins <rcarinha@hotmail.com>
2023-07-11 11:53:56 -0600

    ultralm-13b-GPTQ-4bit-128g.no-act.order refactor

--- app.py
+++ app.py
@@ -5,14 +5,50 @@ from dotenv import load_dotenv
 import chainlit as cl
 import requests
 
-from langchain import PromptTemplate, LLMChain, OpenAI
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from langchain import PromptTemplate, LLMChain, OpenAI, HuggingFacePipeline
+from transformers import AutoTokenizer, pipeline, logging
 from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
 import argparse
 
 load_dotenv()
 
+model_name_or_path = "/home/ryan/repos/text-generation-webui/models/UltraLM-13B-GPTQ"
+model_basename = "ultralm-13b-GPTQ-4bit-128g.no-act.order"
+
+use_triton = False
+
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
+
+model = AutoGPTQForCausalLM.from_quantized(model_name_or_path,
+                                           model_basename=model_basename,
+                                           use_safetensors=True,
+                                           trust_remote_code=False,
+                                           device="cuda:0",
+                                           use_triton=use_triton,
+                                           quantize_config=None)
+
 prompt_template = "{input}?"
+
+input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
+output = model.generate(inputs=input_ids, temperature=0.7, max_new_tokens=512)
+print(tokenizer.decode(output[0]))
+
+# Inference can also be done using transformers' pipeline
+
+# Prevent printing spurious transformers error when using pipeline with AutoGPTQ
+logging.set_verbosity(logging.CRITICAL)
+
+print("*** Pipeline:")
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=512,
+    temperature=0.7,
+    top_p=0.95,
+    repetition_penalty=1.15
+)
+
 
 is_first_question_asked = False
 is_second_question_asked = False
@@ -30,42 +66,18 @@ first_question_answer = ''
 second_question_answer = ''
 third_question_answer = ''
 
-# base_model = AutoModelForCausalLM.from_pretrained(
-#     "/Users/rafaelmarins/PycharmProjects/weather_forecast_ai_chat/llama",
-#     use_safetensors=True,
-# )
+local_llm = HuggingFacePipeline(pipeline=pipe)
 
-# model_id = "bigscience/bloom-1b7"
-#
-# tokenizer = AutoTokenizer.from_pretrained(model_id)
+prompt = PromptTemplate(template=prompt_template, input_variables=["instruction"])
 
-# tokenizer = AutoTokenizer.from_pretrained("/Users/rafaelmarins/PycharmProjects/weather_forecast_ai_chat/llama")
-
-model_name_or_path = "/home/ryan/repos/text-generation-webui/models/falcon-40b"
-# You could also download the model locally, and access it there
-# model_name_or_path = "/path/to/TheBloke_falcon-40b-instruct-GPTQ"
-
-model_basename = "gptq_model-4bit--1g"
-
-use_triton = False
-
-tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
-
-model = AutoGPTQForCausalLM.from_quantized(model_name_or_path,
-                                           model_basename=model_basename,
-                                           use_safetensors=True,
-                                           trust_remote_code=True,
-                                           device="cuda:0",
-                                           use_triton=use_triton,
-                                           quantize_config=None)
+llm_chain = LLMChain(prompt=prompt,
+                     llm=local_llm
+                     )
 
 
 @cl.langchain_factory(use_async=True)
 def main():
-    # llm = OpenAI(temperature=0)
-    # llm = AutoModelForCausalLM.from_pretrained("/Users/rafaelmarins/PycharmProjects/weather_forecast_ai_chat/llama", device_map="auto", load_in_4bit=True)
-    llm = model
-    chain = LLMChain(llm=llm, prompt=PromptTemplate.from_template(prompt_template))
+    chain = llm_chain
     return chain
 
 
